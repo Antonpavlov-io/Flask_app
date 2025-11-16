6d1a0ce6b89836bd9305a7c4b5bbb39e4c85e7f0
Antonpavlov_01 <antonpavlov.srg@gmail.com>
2025-11-16 19:33:01 +0300

    Add files via upload

---
+++ Пример использования шаблонизатора Jinja2.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/example')
+def example():
+    name = 'John'
+    age = 30
+    return render_template('example.html', name=name, age=age)
+
+if __name__ == '__main__':
+    app.run(debug=True)
